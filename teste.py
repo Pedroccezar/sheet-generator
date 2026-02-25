3176a8335232053e767e0aa0b9196bcf89586966
Pedroccezar <pedro.h.c.castilho@gmail.com>
2026-02-25 11:32:04 -0300

    Novo sheets
    Criação de uma nova planilha a partir da da alocação dos membros no sheets de disponibilidade.

--- teste.py
+++ teste.py
@@ -30,8 +30,6 @@ for i in planilha.itertuples():
         sex.append(i.membros)
         plantao.append(i.membros)
         
-    #print(i)
-
 print(f'segunda:{seg}, terca: {ter}, quarta: {qua}, quinta: {qui}, sexta: {sex}')
 
 planilha_def = pd.DataFrame({
@@ -41,5 +39,11 @@ planilha_def = pd.DataFrame({
     'Quinta': pd.Series(qui),
     'Sexta': pd.Series(sex)
 })
+
+planilha_def['Segunda'] = np.where(planilha_def['Segunda'].isna(),'',planilha_def['Segunda'])
+planilha_def['Terça'] = np.where(planilha_def['Terça'].isna(),'',planilha_def['Terça'])
+planilha_def['Quarta'] = np.where(planilha_def['Quarta'].isna(),'',planilha_def['Quarta'])
+planilha_def['Quinta'] = np.where(planilha_def['Quinta'].isna(),'',planilha_def['Quinta'])
+planilha_def['Sexta'] = np.where(planilha_def['Sexta'].isna(),'',planilha_def['Sexta'])
 
 planilha_def.head(10)
