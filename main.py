e0f52bd0994df86ceb2b61c96520cea193433365
Pedroccezar <pedro.h.c.castilho@gmail.com>
2026-02-25 23:07:42 -0300

    Rotação
    Lógica para rotação de membros com base na disponibilidade individual e update no sheets final

---
+++ main.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+from gspread_dataframe import set_with_dataframe
+import gspread
+from google.oauth2.service_account import Credentials
+
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_file(
+    "credencial.json",
+    scopes=scope
+)
+
+client = gspread.authorize(creds)
+sheet = client.open_by_key("15SpGslotuwfz7Fzc6eSFAw690tEsepahhj9vq-3yHw8").sheet1
+
+url = 'https://docs.google.com/spreadsheets/d/17p7v_DldO4hYvko52K_RLS-a9RCNEKuxsMbic--qx2g/export?format=csv'
+planilha = pd.read_csv(url)
+plantaourl = 'https://docs.google.com/spreadsheets/d/15SpGslotuwfz7Fzc6eSFAw690tEsepahhj9vq-3yHw8/export?format=csv'
+planilha_def = pd.read_csv(plantaourl)
+
+seg, ter, qua, qui, sex, plantao = ['Bruno'], ['Luiz'], ['Miguel'], ['Anna Carolina'], ['Nathan'], [] #Se precisar fixar plantão...
+
+for i in planilha.itertuples():
+    if i.segunda:
+        seg.append(i.membros)
+        plantao.append(i.membros)
+        
+    if i.terca and not i.membros in plantao:
+        ter.append(i.membros)
+        plantao.append(i.membros)
+    
+    if i.quarta and not i.membros in plantao:
+        qua.append(i.membros)
+        plantao.append(i.membros)
+        
+    if i.quinta and not i.membros in plantao:
+        qui.append(i.membros)
+        plantao.append(i.membros)
+        
+    if i.sexta and not i.membros in plantao:
+        sex.append(i.membros)
+        plantao.append(i.membros)
+        
+print(f'segunda:{seg}, terca: {ter}, quarta: {qua}, quinta: {qui}, sexta: {sex}')
+
+planilha_def = pd.DataFrame({
+    'Segunda': pd.Series(seg),
+    'Terça': pd.Series(ter),
+    'Quarta': pd.Series(qua),
+    'Quinta': pd.Series(qui),
+    'Sexta': pd.Series(sex)
+})
+
+planilha_def = planilha_def.fillna('')
+set_with_dataframe(sheet, planilha_def)
+
+planilha_def.head(10)
---
+++ update.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+from gspread_dataframe import set_with_dataframe
+import gspread
+from google.oauth2.service_account import Credentials
+
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_file(
+    "credencial.json",
+    scopes=scope
+)
+
+client = gspread.authorize(creds)
+sheet = client.open_by_key("15SpGslotuwfz7Fzc6eSFAw690tEsepahhj9vq-3yHw8").sheet1
+
+url = 'https://docs.google.com/spreadsheets/d/17p7v_DldO4hYvko52K_RLS-a9RCNEKuxsMbic--qx2g/export?format=csv'
+planilha = pd.read_csv(url)
+plantaourl = 'https://docs.google.com/spreadsheets/d/15SpGslotuwfz7Fzc6eSFAw690tEsepahhj9vq-3yHw8/export?format=csv'
+planilha_def = pd.read_csv(plantaourl)
+
+dias_nomes = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta']
+dias_slugs = ['segunda', 'terca', 'quarta', 'quinta', 'sexta'] 
+
+ultima_escala = {}
+for dia in dias_nomes:
+    if dia in planilha_def.columns:
+        pessoas = planilha_def[dia].dropna().tolist()
+        for p in pessoas:
+            if p: ultima_escala[p] = dias_nomes.index(dia)
+
+novas_listas = {
+    'segunda': ['Bruno'], 
+    'terca': ['Luiz'], 
+    'quarta': ['Miguel'], 
+    'quinta': ['Anna Carolina'], 
+    'sexta': ['Nathan']
+}
+plantao_geral = ['Bruno', 'Luiz', 'Miguel', 'Anna Carolina', 'Nathan'] #Se precisar fixar plantão...
+
+for i in planilha.itertuples():
+    membro = i.membros
+    if membro in plantao_geral:
+        continue 
+    
+    index_anterior = ultima_escala.get(membro, -1)
+    alvo_index = (index_anterior + 1) % 5
+    
+    alocado = False
+    for tentativa in range(5):
+        atual_index = (alvo_index + tentativa) % 5
+        coluna_disponibilidade = dias_slugs[atual_index]
+        
+        if getattr(i, coluna_disponibilidade, False):
+            novas_listas[coluna_disponibilidade].append(membro)
+            plantao_geral.append(membro)
+            alocado = True
+            break
+
+df_final = pd.DataFrame({
+    'Segunda': pd.Series(novas_listas['segunda']),
+    'Terça':   pd.Series(novas_listas['terca']),
+    'Quarta':  pd.Series(novas_listas['quarta']),
+    'Quinta':  pd.Series(novas_listas['quinta']),
+    'Sexta':   pd.Series(novas_listas['sexta'])
+}).fillna('')
+
+set_with_dataframe(sheet, df_final)
+print("Planilha atualizada!")
+
